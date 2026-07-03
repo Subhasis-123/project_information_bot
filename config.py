db22c789c3707d254cac0fe0f89bffc68872c057
acharjee ka college <acharjeesubhasis58@gmail.com>
2026-07-03 19:10:46 +0530

    Updated Streamlit secret handling

--- config.py
+++ config.py
@@ -3,7 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+try:
+    import streamlit as st
+except ImportError:
+    st = None
+
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# If running on Streamlit Cloud, read from Secrets
+if not GOOGLE_API_KEY and st is not None:
+    GOOGLE_API_KEY = st.secrets.get("GOOGLE_API_KEY")
 
 KNOWLEDGE_PATH = "knowledge"
 
